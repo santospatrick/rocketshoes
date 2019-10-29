0a90e3b5a96810f01bccce5e706bf6e9b776acb6
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-28 22:25:16 -0300

    adiciona lib para icones e header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
     return (
         <Router>
+            <Header />
             <Routes />
             <GlobalStyle />
         </Router>

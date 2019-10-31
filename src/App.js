bcda45fa328ea7487211883f6274ab25e7479335
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-30 22:23:52 -0300

    adiciona reactotron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/reactotron';
+
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';

52d2bfa3f3995b67f9c331709d588b4072ae2c28
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-30 21:54:06 -0300

    adiciona axios e formatacao intl

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,85 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
 function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getProduts() {
+            const response = await api.get('products');
+
+            const data = response.data.map(product => ({
+                ...product,
+                priceFormatted: formatPrice(product.price),
+            }));
+
+            setProducts(data);
+        }
+
+        getProduts();
+    }, []);
+
     return (
         <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis mto legal</strong>
-                <span>R$120,90 </span>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt={product.title} />
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis mto legal</strong>
-                <span>R$120,90 </span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis mto legal</strong>
-                <span>R$120,90 </span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis mto legal</strong>
-                <span>R$120,90 </span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>adicionar ao carrinho</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                    alt="Tênis"
-                />
-                <strong>Tênis mto legal</strong>
-                <span>R$120,90 </span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>adicionar ao carrinho</span>
-                </button>
-            </li>
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart size={16} color="#fff" /> 3
+                        </div>
+                        <span>adicionar ao carrinho</span>
+                    </button>
+                </li>
+            ))}
         </ProductList>
     );
 }

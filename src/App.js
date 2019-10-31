387abb1a9028f3320d0a8bf3875ad3dbbc7791b6
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-31 00:23:07 -0300

    adiciona navegacao pelo history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -9,12 +9,13 @@ import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 
+import history from './services/history';
 import store from './store';
 
 function App() {
     return (
         <Provider store={store}>
-            <Router>
+            <Router history={history}>
                 <Header />
                 <Routes />
                 <GlobalStyle />
